9f317fa8fdd939e0fb0ea8f8fc7cf44d7d5cfcc0
Greg Mitchell <g2mitchell3003@gmail.com>
2020-09-23 10:40:26 -0600

    WIP code summary method and helper

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -19,4 +19,27 @@ class CookBook
   def date
     Time.now.strftime("%m%d%y")
   end
+
+  def summary_ingredients(ingredients,recipe)
+    ingredients.reduce([]) do |summary_array, ingredient|
+      summary_hash = {}
+      summary_hash[:ingredient] = ingredient.name
+      summary_hash[:amount] = "#{recipe.ingredients_required[ingredient]} #{ingredient.unit}"
+      summary_array << summary_hash
+      summary_array
+    end
+  end
+
+
+  def summary
+    @recipes.reduce([]) do |summ, recipe|
+      recipe_hash = {}
+      details_hash = {}
+      details_hash[:ingredients] = summary_ingredients(recipe.ingredients, recipe)
+      recipe_hash[:name] = recipe.name
+      recipe_hash[:details] = details_hash
+      summ << recipe_hash
+      summ
+    end
+  end
 end
