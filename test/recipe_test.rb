4720b93d74e06e44520d0f2226b992270fdae627
Greg Mitchell <g2mitchell3003@gmail.com>
2020-09-23 09:26:22 -0600

    Create test for initialization of recipe class

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -2,9 +2,12 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/ingredient'
 require './lib/pantry'
+require './lib/recipe'
 
 class RecipeTest < Minitest::Test
   def test_it_exists
-    
+    recipe1 = Recipe.new(" ")
+
+    assert_instance_of Recipe, recipe1
   end
 end
