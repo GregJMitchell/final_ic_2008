6dec47c5a595d08f689ae65b72b81dc04762fad1
Greg Mitchell <g2mitchell3003@gmail.com>
2020-09-23 09:57:26 -0600

    Create test for enough ingredients for method

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -31,4 +31,22 @@ class PantryTest < Minitest::Test
 
     assert_equal 15, pantry.stock_check(ingredient1)
   end
+
+  def test_it_can_see_if_enough_ingredients_for
+    pantry = Pantry.new
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    recipe1 = Recipe.new("Mac and Cheese")
+    recipe1.add_ingredient(ingredient1, 2)
+    recipe1.add_ingredient(ingredient2, 8)
+    ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+    ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
+    recipe2 = Recipe.new("Cheese Burger")
+    recipe2.add_ingredient(ingredient1, 2)
+    recipe2.add_ingredient(ingredient3, 4)
+    recipe2.add_ingredient(ingredient4, 1)
+    pantry.restock(ingredient1, 5)
+    pantry.restock(ingredient1, 10)
+    assert_false enough_ingredients_for?(recipe1)
+  end
 end
