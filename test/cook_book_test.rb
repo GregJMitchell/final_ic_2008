787cf0cc51c0389c81be88a1b63093d9eddc9f5c
Greg Mitchell <g2mitchell3003@gmail.com>
2020-09-23 09:24:56 -0600

    Create recipe and cook book test files

---
+++ test/cook_book_test.rb
@@ -0,0 +1,10 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/cook_book'
+require './lib/recipe'
+
+class CookBookTest < Minitest::Test
+  def test_it_exists
+
+  end
+end
---
+++ test/recipe_test.rb
@@ -0,0 +1,10 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ingredient'
+require './lib/pantry'
+
+class RecipeTest < Minitest::Test
+  def test_it_exists
+    
+  end
+end
